b25806b5fcddc6348fe79151d6641f412bca3d48
Nigon001 <ferreiraviniciussdi@gmail.com>
2018-05-28 17:11:26 -0300

    classes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+#include "problem_1689.h"
+
 
 int knapsack(int W, int wt[], int b[], int n)
 {
@@ -61,57 +63,8 @@ int knapsack(int W, int wt[], int b[], int n)
 // wt[] - vetor de pontos
 // b[]  - vetor de beneficios
 
-int modulo(int a, int b){
-    if(a > b)
-        return a - b;
-    return b - a;
-}
-
-int mochila_pt_br(int W, int wt[], int b[], int n)
-{
-
-        int soma = 0;
-        int maior = 0;
 
 
-        for(int i = 0; i < n; i++)
-        {
-            cout << wt[i] << " & " <<  wt[i-1] << "=" << modulo(wt[i], wt[i-1]) << endl;
-
-            // preparar melhor as combinacoes
-
-            if (modulo(wt[i], wt[i-1]) >= W){
-                cout << "pontos validos" << wt[i] << " - " <<  wt[i-1] << "=" << modulo(wt[i], wt[i-1]) << " >= " << b[i] + b[i-1]  << endl;
-                soma += b[i] + b[i-1];
-            }
-
-    }
-    // retorna o valor máximo colocado na mochila - V[n][W]
-    return soma;
-}
-
-int mochila_br_combinations(int W, int wt[], int b[], int n)
-{
-
-    int soma = 0;
-    int maior = 0;
-
-
-    for(int i = 0; i < n; i++)
-    {
-        cout << wt[i] << " & " <<  wt[i-1] << "=" << modulo(wt[i], wt[i-1]) << endl;
-
-        // preparar melhor as combinacoes
-
-        if (modulo(wt[i], wt[i-1]) >= W){
-            cout << "pontos validos" << wt[i] << " - " <<  wt[i-1] << "=" << modulo(wt[i], wt[i-1]) << " >= " << b[i] + b[i-1]  << endl;
-            soma += b[i] + b[i-1];
-        }
-
-    }
-    // retorna o valor máximo colocado na mochila - V[n][W]
-    return soma;
-}
 
 void entrada(){
 
@@ -134,15 +87,32 @@ void entrada(){
     int wt[] = {1,2,3};
 
     // obtém o máximo valor que pode ser colocado na mochila
-    int max_valor = mochila_pt_br(W, wt, b, n);
+    int max_valor = knapsack(W, wt, b, n);
     cout << "Valor maximo: " << max_valor << endl;
+}
+
+void entrada_de_classe(){
+    problem_1689 *mochila;
+    mochila = new problem_1689;
+    mochila->about();
+
+    int W = 5; // limite de distancia
+
+    int n = 5; // número de elementos
+
+    int b[] = {5,20,10,15,25}; // vetor com os valores (benefício) de cada elemento
+
+    int pontos[] = {1,5,10,15,17}; // vetor com os pesos de cada elemento
+
+
+    mochila->mochila_radares(W, pontos, b, n);
 }
 
 int main(int argc, char *argv[])
 {
 
-
-    entrada();
+    //entrada();
+    entrada_de_classe();
 
     return 0;
 }
