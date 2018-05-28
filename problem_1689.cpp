2624b9d0ca1a7059c5fab71d905947d591b222ef
Nigon001 <ferreiraviniciussdi@gmail.com>
2018-05-28 19:26:20 -0300

    classes problem_1689 funcionando com vector of struct

--- problem_1689.cpp
+++ problem_1689.cpp
@@ -18,7 +18,7 @@ int problem_1689::distancia(int a, int b){
 bool problem_1689::validar_entradas(int linha, int coluna){
 
     for(int i=0; i<comb.size(); i++){
-        if((linha == comb[i].linha and coluna == comb[i].coluna)or(coluna == comb[i].linha and linha == comb[i].coluna)){
+        if((linha == comb[i].linha and coluna == comb[i].coluna) or (coluna == comb[i].linha and linha == comb[i].coluna)){
             cout << " Valores ja possuem ";
             cout << linha << ":" << coluna <<" => linha = " << comb[i].linha << " coluna = " << comb[i].coluna << endl;
             return false;
@@ -30,33 +30,34 @@ bool problem_1689::validar_entradas(int linha, int coluna){
 
 void problem_1689::ver_combinacoes(){
     for(int i=0; i<comb.size(); i++){
-        cout << i <<": linha = " << comb[i].linha << " coluna = " << comb[i].coluna << endl;
+        cout << i <<" :[" << comb[i].linha << "] [" << comb[i].coluna << "] = " << comb[i].beneficio << endl;
     }
-
 }
 
 
 int problem_1689::mochila_radares(int W, int pontos[], int b[], int n)
 {
 
-    int soma = 0;
+     int soma = 0;
 
      int indice = 0;
 
+     ver_combinacoes();
 
     for(int i=0;i<n;i++){
         for(int j =0;j<W;j++){
 
             if(i!=j){
-                int a = pontos[i];
-                int b = pontos[j];
-                if(distancia(a,b) >  W){
-                    cout << "Candidato com distncia "<< a <<"  " << b << " " << distancia(pontos[i], pontos[j]) << endl;
+                int x = pontos[i];
+                int y = pontos[j];
+                if(distancia(x,y) >  W){
+                    //cout << "Candidato com distncia "<< a <<"  " << b << " " << distancia(pontos[i], pontos[j]) << endl;
                     // empilhar as combinacoes de distancia
                     if(validar_entradas(i,j)){
                         comb.emplace_back();
                         comb[indice].linha = i;
                         comb[indice].coluna = j;
+                        comb[indice].beneficio = b[i] + b[j];
                         indice += 1;
                     }
                 }
